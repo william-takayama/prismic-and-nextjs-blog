36451e828c82876d77f8598d03ffab0613b6d9a5
William Takayama <william-sobreira@hotmail.com>
2021-08-03 19:43:03 -0300

    feat: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.container}>
+      <Link href="/">
+        <a>
+          <Image src="/images/logo.svg" alt="logo" width={240} height={25} />
+        </a>
+      </Link>
+    </header>
+  );
 }
